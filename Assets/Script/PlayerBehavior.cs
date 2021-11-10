d55d4b3a0b1b38f4611dc2ef26e8d39e49ebbbd6
Charles_Viau <charles_violon@hotmail.com>
2021-11-10 11:11:05 -0500

    player death

--- Assets/Script/PlayerBehavior.cs
+++ Assets/Script/PlayerBehavior.cs
@@ -9,5 +9,19 @@ public class PlayerBehavior : MonoBehaviour
 {
     private Vector2 movVector;
     [HideInInspector] public int bonusRange { get; set; } = 0;
+    private HumanoidBehavior hb;
+
+    private void Awake()
+    {
+        hb = GetComponent<HumanoidBehavior>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Physics2D.OverlapBox(transform.position,new Vector2(1,1), 0, LayerMask.GetMask("Ghost", "Ennemi")))
+        {
+            hb.SetInactive();
+        }
+    }
 
 }
